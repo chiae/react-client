48f21a19353d2b7026d66a516bb53f461730c699
Emanuel Chiaburu <emanchiaburu@hotmail.com>
2026-03-11 09:43:11 -0400

    Cleaned up .js files

--- src/layouts/AppLayout.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Outlet } from "react-router-dom";
-import { AppNav } from "@/components/AppNav";
-export function AppLayout() {
-    return (_jsxs("div", { className: "min-h-screen bg-base-200 text-base-content", children: [_jsx(AppNav, {}), _jsxs("main", { className: "mx-auto max-w-6xl px-6 pb-8", children: [_jsx(Outlet, {}), " "] })] }));
-}
